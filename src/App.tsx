cd612fdb39e7d3557537cbd768d391129cb68dab
Erik <fereem@Fereem.home>
2024-08-09 20:19:56 +0200

    huh

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import Card from './components/Card'
+import { fetchMovieChanges } from './utils/api'; // Adjust the import path based on your project structure
 
 function App() {
 
-
+  fetchMovieChanges();
+  
   return (
     <>
       <Card />
--- src/utils/api.js
+++ src/utils/api.js
@@ -2,21 +2,23 @@ import axios from 'axios';
 
 const options = {
   method: 'GET',
-  url: 'https://api.themoviedb.org/3/movie/changes',
-  params: {page: '1'},
+  url: 'https://api.themoviedb.org/3/genre/movie/list',
+  params: {language: 'en'},
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWU4YzY5YjdhOTNkYmU5MTZjYTg2MzhmZDFlY2ZiMiIsIm5iZiI6MTcyMzIyMTQ4NS4wMDM3NTQsInN1YiI6IjY2YjYxYzI2N2VlOWYzYjMwZjA3MTRjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mlqlUhoIgoZKxfLiDBRcPWHS6gCuLiceFZwHwz-hoYc'
   }
 };
+const movieList = () => {
 
-export const fetchMovieChanges = () => {
-  return axios
+ axios
     .request(options)
     .then(function (response) {
-      console.log(response.data);
+        console.log(response.data);
     })
     .catch(function (error) {
-      console.error(error);
+        console.error(error);
     });
-};
+}
+
+movieList();

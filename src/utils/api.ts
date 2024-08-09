81fe39c5797e71564ca89fdfdad8cf02408a4456
Erik <fereem@Fereem.home>
2024-08-09 18:06:23 +0200

    first commit

---
+++ src/utils/api.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const fetchGames = async () => {
+  try {
+    const response = await axios.post('https://api.igdb.com/v4/games', 
+      "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,collections,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;",
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Client-ID': 'Client ID',
+          'Authorization': 'Bearer access_token',
+        }
+      }
+    );
+
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+fetchGames();

57e30d9e94851488cefd0f27091840364268c48f
erik <fereem@desktop-c8v7606.home>
2024-08-10 12:16:40 +0200

    teches

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,64 @@
 
 import './App.css'
 import Card from './components/Card'
-import { fetchMovieChanges } from './utils/api'; // Adjust the import path based on your project structure
+import { useState, useEffect } from 'react';
+import { fetchMovieGenres, Genre, fetchMovieTopRated, TopRated } from './utils/api'; // Adjust the import path based on your project structure
+
 
 function App() {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [topRated, setTopRated] = useState<TopRated[]>([]);
 
-  fetchMovieChanges();
+  useEffect(() =>{
+    const getTopRated = async () => {
+      try {
+        const data = await fetchMovieTopRated();
+        setTopRated(data.TopRateds);
+      } catch (error){
+        console.error(error);
+      }
+    };
+    getTopRated();
+  },[]);
+
+  useEffect(() => {
+    const getGenres = async () => {
+      try {
+        const data = await fetchMovieGenres();
+        setGenres(data.genres);
+      } catch (error){
+        console.error(error);
+      }
+    };
+    getGenres();
+  }, []);
+
   
   return (
     <>
-      <Card />
+      <h1 className='text-xl'>Movie Genres</h1>
+      <ul>
+        {genres.map(genre => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+
+      <h1 className='text-3xl'>TopRatedMovies</h1>
+      <ul>
+        {topRated.map(TR => (
+          <li key={TR.id}>
+            <div>'ADULT: '{TR.adult}</div>
+            <div>'backdrop_path: '{TR.backdrop_path}</div>
+            
+            <div>'id: '{TR.id}</div>
+            <div>'original_language: ' {TR.original_language}</div>
+            <div>'title: ' {TR.title}</div>
+          </li>
+        ))}
+      </ul>
+
     </>
-  )
+  );
 }
 
 export default App
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,22 +1,59 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+const apiKey = import.meta.env.VITE_API_KEY;
+//////////////////////////////////////////////////////////
+export interface Genre {
+  id: number;
+  name: string;
+}
 
-const options = {
+interface GenreRes {
+  genres: Genre[];
+}
+/////////////////////////////////////////////////////////
+
+export interface TopRated {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: Genre[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  realease_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TopRatedRes{
+  result: TopRated[];
+}
+///////////////////////////////////////////////////////////
+const baseConfig: AxiosRequestConfig = {
   method: 'GET',
-  url: 'https://api.themoviedb.org/3/movie/changes',
-  params: {page: '1'},
+  params: { language: 'en' },
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWU4YzY5YjdhOTNkYmU5MTZjYTg2MzhmZDFlY2ZiMiIsIm5iZiI6MTcyMzIyMTQ4NS4wMDM3NTQsInN1YiI6IjY2YjYxYzI2N2VlOWYzYjMwZjA3MTRjMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mlqlUhoIgoZKxfLiDBRcPWHS6gCuLiceFZwHwz-hoYc'
+    Authorization: `Bearer ${apiKey}`
   }
 };
 
-export const fetchMovieChanges = () => {
-  return axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-};
+const createConfig = (url: string): AxiosRequestConfig => ({
+  ...baseConfig,
+  url,
+})
+
+export const fetchMovieGenres = async (): Promise<GenreRes> => {
+  const config = createConfig('https://api.themoviedb.org/3/genre/movie/list');
+  const response: AxiosResponse<GenreRes> = await axios.request<GenreRes>(config);
+  return response.data;
+}
+/////
+export const fetchMovieTopRated = async () : Promise<TopRatedRes> => {
+  const config = createConfig('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1');
+  const response: AxiosResponse<TopRatedRes> = await axios.request<TopRatedRes>(config);
+  return response.data;
+}
